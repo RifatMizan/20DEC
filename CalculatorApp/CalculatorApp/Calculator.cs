a80983d9810b09fa7b8ac7fe656d577c275974ae
RifatMizan <rifat.mizan@gmail.com>
2014-12-28 20:42:31 +0600

    updated

---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public double firstNo;
+        public double secondNo;
+        public double result;
+
+        public double Add()
+        {
+            return firstNo + secondNo;
+        }
+
+        public double Subtract()
+        {
+            return firstNo - secondNo;
+        }
+
+        public double Divide()
+        {
+            return firstNo / secondNo;
+        }
+
+        public double Multiply()
+        {
+            return firstNo * secondNo;
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/CalculatorUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorUI : Form
+    {
+        public CalculatorUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
